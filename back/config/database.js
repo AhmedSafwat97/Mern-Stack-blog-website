76710ecedfaadcc341faafe4dca8617d6ed93c48
AhmedSafwat97 <xppahmad@gmail.com>
2023-10-18 23:15:12 +0300

    final

--- back/config/database.js
+++ back/config/database.js
@@ -8,6 +8,7 @@ const dbConnection = () => {
     useUnifiedTopology: true,
   });
   
+  
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'Connection error:'));
   db.once('open', () => {
