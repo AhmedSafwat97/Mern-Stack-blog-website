122c740b3772899b0f4553e81dffc503f60a0dc9
AhmedSafwat97 <xppahmad@gmail.com>
2024-08-20 21:20:12 +0300

    .....

--- back/config/configFile.js
+++
@@ -1,2 +0,0 @@
-exports.ConnectDatabase = "mongodb+srv://blogging:12345@cluster0.z2jl82p.mongodb.net/"
-
--- client/src/Componants/MainLink.jsx
+++
@@ -1,7 +0,0 @@
-const MailLink = "http://localhost:5000"
-
-export default MailLink
-
-
-
-// https://social-media-blog-n136.onrender.com
